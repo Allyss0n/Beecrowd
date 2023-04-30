78b135e72bd711740661c61d33962b6587f799ea
allysson <allyss0n@aluno.fapce.edu.br>
2023-04-30 17:17:37 -0300

    1044

---
+++ Python/1044.py
@@ -0,0 +1,14 @@
+a, b = input().split()
+a = int(a)
+b = int(b)
+
+if a <= b:
+    if (b%a == 0):
+        print("Sao Multiplos")
+    else:
+        print("Nao sao Multiplos")
+elif a > b :
+    if (a%b == 0):
+        print("Sao Multiplos")
+    else:
+        print("Nao sao Multiplos")
