597f2b4e3f6958add4cb4ac72dabe2ff3122e440
allysson <allyss0n@aluno.fapce.edu.br>
2023-05-02 10:44:55 -0300

    1020,1060,1061

---
+++ Python/1060.py
@@ -0,0 +1,13 @@
+v1 = float(input())
+v2 = float(input())
+v3 = float(input())
+v4 = float(input())
+v5 = float(input())
+v6 = float(input())
+r=0
+v =[v1,v2,v3,v4,v5,v6]
+for i in (v):
+    if i>0:
+        r+=1
+
+print ("{} valores positivos".format(r))
